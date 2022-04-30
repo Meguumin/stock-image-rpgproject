4a7e3ae4d2b59b62fb7e04038a36a6173fe1436d
Aiden <Aiden@DESKTOP-8C089JU>
2022-04-30 10:31:43 -0700

    Add project files.

---
+++ RPGTest Winform/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace RPGTest_Winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
+            this.button1.Location = new System.Drawing.Point(232, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(232, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Roll";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(154, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(369, 304);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
+            this.label1.Location = new System.Drawing.Point(529, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Level:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
+            this.label2.Location = new System.Drawing.Point(629, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 39);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
+            this.label3.Location = new System.Drawing.Point(616, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 39);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
+            this.label4.Location = new System.Drawing.Point(529, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 39);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Dice:";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 17.25F);
+            this.button2.Location = new System.Drawing.Point(232, 433);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(232, 45);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Buy Dice ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(232, 484);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(232, 45);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(96, 625);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(285, 26);
+            this.progressBar1.TabIndex = 8;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
+            this.label5.Location = new System.Drawing.Point(-5, 615);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 39);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "EXP:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
+            this.label6.Location = new System.Drawing.Point(409, 343);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 39);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "0";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
+            this.label7.Location = new System.Drawing.Point(263, 341);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(155, 39);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Damage:";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
+            this.label8.Location = new System.Drawing.Point(387, 615);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(128, 39);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Money:";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.25F);
+            this.label9.Location = new System.Drawing.Point(504, 617);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(36, 39);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 15000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 663);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Rpg";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ RPGTest Winform/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RPGTest_Winform
+{
+    public partial class Form1 : Form
+    {
+        
+        float money = 100000;
+        float price_dice = 20;
+        bool monster_spawned = false;
+        int dice_damagelimit = 6;
+        int dice = 0;
+        int exp;
+        int levels = 1;
+        string dollarsign = "$";
+        string buydice = "Buy Dice";
+        string space = " ";
+        string disabled = "Disabled";
+        Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Text = levels.ToString();
+            label3.Text = dice.ToString();
+            button2.Text = buydice + space + price_dice + dollarsign;
+        }
+        //roll
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dice_damagelimit = dice_damagelimit * dice;
+            int dicedamage = rand.Next(dice, dice_damagelimit);   
+            label6.Text = dicedamage.ToString();    
+
+        }
+        //Buy Dice Button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            if (money > price_dice)
+            {
+                money = money - price_dice;
+                dice++;
+                label3.Text = dice.ToString();
+                price_dice = 1.7f * price_dice;
+
+                  price_dice = (float)Math.Round(price_dice);                                         
+                button2.Text = buydice + space + price_dice + dollarsign;
+                if (dice == 15)
+                {
+                    button2.Enabled = false;
+                    button2.Text = disabled;
+                }
+            }
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label9.Text = money.ToString();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if(monster_spawned == false)
+            {
+                
+               int monster_spawn = rand.Next(1, 3);
+                if(monster_spawn == 3)
+                {
+                    //spawn monster
+                    monster_spawned = !monster_spawned;
+                }                
+            }
+            
+        }
+
+
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
